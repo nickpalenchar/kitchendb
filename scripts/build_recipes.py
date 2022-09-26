1720830eb036d663ee3b57f0b1eab04faf6ec3c4
Nick Palenchar <nickpal@nickpalenchar.com>
2022-09-26 11:08:32 -0400

    wip

---
+++ scripts/build_recipes.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+def build():
+  """
+  build takes all json files in data/recipes and builds them into content pages.
+
+  JSON files must all be UpperCammelCase
+  """
+  for file in os.listdir('data/recipes'):
+    validate_file_name(file)
+    mkdown_name = file.replace(".json", ".md")
+
+    print(camel_to_snake_case(mkdown_name))
+
+    #subprocess.run(["hugo", "new", "--kind", "recipes", f"content/"])
+
+
+def validate_file_name(name):
+  if name[0].islower():
+    raise Exception("Name must begin with upper case letter")
+  if not name.endswith('.json'):
+    raise Exception("name must be .json extension")
+  return name
+
+def camel_to_snake_case(name):
+  return ''.join([name[0].lower()] + [c if c.islower() or c == "." else f'-{c.lower()}' for c in name[1:]])
+
+if __name__ == '__main__':
+  build()
