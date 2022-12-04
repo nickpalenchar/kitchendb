e56ba2dc4181dad545635e839c7ddbd6739999ef
Nick Palenchar <nickpal@nickpalenchar.com>
2022-12-04 08:54:51 -0500

    deploy thumbnails with images

--- scripts/create_thumbnails.py
+++ scripts/create_thumbnails.py
@@ -6,17 +6,19 @@ from PIL import Image
 import os
 import sys
 
-def main(overwrite=False):
-  for file in os.listdir("../static/images"):
-    if file == "thumbnail":
-      continue
-    if os.path.exists(f"../static/images/{file}") and not overwrite:
-      continue
 
-    im = Image.open(f"../static/images/{file}").copy()
-    factor = max(im.size) / 256
-    im.thumbnail([s // factor for s in im.size])
-    im.save(f"../static/images/thumbnail/{file}")
+def create(overwrite=False):
+    for file in os.listdir("../static/images"):
+        if file == "thumbnail":
+            continue
+        if os.path.exists(f"../static/images/{file}") and not overwrite:
+            continue
 
-if __name__ == '__main__':
-  main(overwrite=len(sys.argv) > 1)
+        im = Image.open(f"../static/images/{file}").copy()
+        factor = max(im.size) / 256
+        im.thumbnail([s // factor for s in im.size])
+        im.save(f"../static/images/thumbnail/{file}")
+
+
+if __name__ == "__main__":
+    create(overwrite=len(sys.argv) > 1)
